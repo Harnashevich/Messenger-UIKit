71cff80eb8796b48e1acade0514bfdffad4e99fb
Andrei <garnash1313@gmail.com>
2024-09-05 14:34:15 +0300

    Initial Commit

---
+++ MessengerUIKit2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerUIKit2024
+//
+//  Created by Andrei Harnashevich on 5.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
